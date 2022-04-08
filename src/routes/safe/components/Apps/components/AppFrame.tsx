85a4e5947d95bdfc635e676c8a358985bf9f38a9
Diogo Soares <diogo.soares@gnosis.pm>
2022-04-08 17:49:10 +0200

    feat: display top ranked apps

--- src/routes/safe/components/Apps/components/AppFrame.tsx
+++ src/routes/safe/components/Apps/components/AppFrame.tsx
@@ -127,6 +127,7 @@ const AppFrame = ({ appUrl }: Props): ReactElement => {
           ...trackData[currentApp.id],
           timestamp: Date.now(),
           openCount: currentOpenCount ? ++currentOpenCount : 1,
+          txCount: trackData[currentApp.id]?.txCount || 0,
         },
       })
       timer.current = window.setTimeout(() => {
